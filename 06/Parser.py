2b1422860834b3d965a7345508653c533f880f9d
Galo <gofer1505@gmail.com>
2021-08-04 17:59:34 +0300

    Parser

--- 06/Parser.py
+++ 06/Parser.py
@@ -5,6 +5,7 @@ and as allowed by the Creative Common Attribution-NonCommercial-ShareAlike 3.0
 Unported License (https://creativecommons.org/licenses/by-nc-sa/3.0/).
 """
 import typing
+from tables import command_types, dest_table, comp_table, jump_table
 
 
 class Parser:
@@ -22,8 +23,17 @@ class Parser:
         """
         # Your code goes here!
         # A good place to start is:
-        # input_lines = input_file.read().splitlines()
-        pass
+        self.input_lines = input_file.read().splitlines()
+        self._remove_whitespace_lines()
+        self.line_index = 0
+        self.curr_command = self.input_lines[self.line_index]
+
+    @staticmethod
+    def _is_line_whitespace(line: str):
+        return line == '' or line.strip().split("//")[0] == ''
+
+    def _remove_whitespace_lines(self):
+        self.input_lines = list(filter(Parser._is_line_whitespace, self.input_lines))
 
     def has_more_commands(self) -> bool:
         """Are there more commands in the input?
@@ -31,15 +41,14 @@ class Parser:
         Returns:
             bool: True if there are more commands, False otherwise.
         """
-        # Your code goes here!
-        pass
+        return self.line_index <= (len(self.input_lines) - 1)
 
     def advance(self) -> None:
         """Reads the next command from the input and makes it the current command.
         Should be called only if has_more_commands() is true.
         """
-        # Your code goes here!
-        pass
+        self.line_index += 1
+        self.curr_command = self.input_lines[self.line_index]
 
     def command_type(self) -> str:
         """
@@ -49,8 +58,7 @@ class Parser:
             "C_COMMAND" for dest=comp;jump
             "L_COMMAND" (actually, pseudo-command) for (Xxx) where Xxx is a symbol
         """
-        # Your code goes here!
-        pass
+        return command_types[self.curr_command[0]]
 
     def symbol(self) -> str:
         """
@@ -59,8 +67,10 @@ class Parser:
             (Xxx). Should be called only when command_type() is "A_COMMAND" or 
             "L_COMMAND".
         """
-        # Your code goes here!
-        pass
+        if self.command_type() == "A_COMMAND":
+            return self.curr_command[1:]
+        else:
+            return self.curr_command.strip("()")
 
     def dest(self) -> str:
         """
@@ -68,8 +78,12 @@ class Parser:
             str: the dest mnemonic in the current C-command. Should be called 
             only when commandType() is "C_COMMAND".
         """
-        # Your code goes here!
-        pass
+
+        if "=" in self.curr_command:
+            dest_symbol = self.curr_command.split("=")[0]
+            return dest_table[dest_symbol]
+        else:
+            return "000"
 
     def comp(self) -> str:
         """
@@ -77,8 +91,14 @@ class Parser:
             str: the comp mnemonic in the current C-command. Should be called 
             only when commandType() is "C_COMMAND".
         """
-        # Your code goes here!
-        pass
+        comp = self.curr_command.split("=")[1].split(";")[0]
+        if "M" in comp:
+            a = "1"
+            dummy_comp = comp.replace("M", "B")
+        else:
+            a = "0"
+            dummy_comp = comp.replace("D", "B")
+        return comp_table[dummy_comp]
 
     def jump(self) -> str:
         """
@@ -86,5 +106,8 @@ class Parser:
             str: the jump mnemonic in the current C-command. Should be called 
             only when commandType() is "C_COMMAND".
         """
-        # Your code goes here!
-        pass
+        if ";" in self.curr_command:
+            jump_symbol = self.curr_command.split(";")[1]
+            return jump_table[jump_symbol]
+        else:
+            return "000"
