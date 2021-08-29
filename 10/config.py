f752d847383d99dcd8814877ec92451eb6226662
Amit Keinan <amitk2612@gmail.com>
2021-08-29 09:55:19 +0300

    update patterns

--- 10/config.py
+++ 10/config.py
@@ -4,7 +4,9 @@ KEY_WORDS = ['class', 'constructor', 'function', 'method', 'field', 'static', 'v
              'true', 'false', 'null', 'this', 'let', 'do', 'if', 'else', 'while', 'return']
 SYMBOLS = ['{', '}', '(', ')', '[', ']', '.', ',', ';', '+', '-', '*', '/', '&', '|', '<', '>', '=', '~']
 
-STRING_CONST_PATTERN = '"[^\\n]*?"'
+STRING_CONST_PATTERN = '"[^\\n"]*?"'
+IDENTIFIER_PATTERN = '[a-zA-Z_][a-zA-Z0-9_]*'
+
 
 # comments removing
 
