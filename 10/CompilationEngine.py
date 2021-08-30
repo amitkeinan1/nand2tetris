f9ee8a940d2ada398832077e2032a9eca238ec9c
Galo <gofer1505@gmail.com>
2021-08-30 21:11:16 +0300

    annotations and return in compile term

--- 10/CompilationEngine.py
+++ 10/CompilationEngine.py
@@ -4,7 +4,7 @@ https://www.nand2tetris.org (Shimon Schocken and Noam Nisan, 2017)
 and as allowed by the Creative Common Attribution-NonCommercial-ShareAlike 3.0 
 Unported License (https://creativecommons.org/licenses/by-nc-sa/3.0/).
 """
-from typing import List
+from typing import List, Union
 
 from lxml import etree
 from lxml.etree import Element
@@ -34,7 +34,7 @@ class CompilationEngine:
 
     # helper methods
 
-    def _add_curr_token(self) -> List[Element]:
+    def _add_curr_token(self) -> Union[List[Element], None]:
         if self.tokenizer.curr_index == len(
                 self.tokenizer.tokens):  # TODO: this is kinda patch but this is from the last stuff t fix, unless it causes bugs
             return None
@@ -43,7 +43,7 @@ class CompilationEngine:
         self.tokenizer.advance()  # TODO: maybe sometimes we want to take it back? I think methods like compile_safely are the key
         return [token_element]
 
-    def _add_token_if(self, expected_type=None, expected_token=None) -> List[Element]:
+    def _add_token_if(self, expected_type=None, expected_token=None) -> Union[List[Element], None]:
         if self.tokenizer.curr_index == len(
                 self.tokenizer.tokens):  # TODO: this is kinda patch but this is from the last stuff t fix, unless it causes bugs
             return None
@@ -53,13 +53,13 @@ class CompilationEngine:
         else:
             return None
 
-    def _add_token_if_or_compile(self, expected_type, expected_token, compile_method) -> List[Element]:
+    def _add_token_if_or_compile(self, expected_type, expected_token, compile_method) -> Union[List[Element], None]:
         elements_to_add = self._add_token_if(expected_type, expected_token)
         if not elements_to_add:
             elements_to_add = compile_method()
         return elements_to_add
 
-    def _add_token_if_or(self, expected_types=None, expected_tokens=None) -> List[Element]:
+    def _add_token_if_or(self, expected_types=None, expected_tokens=None) -> Union[List[Element], None]:
         if expected_types is None and expected_tokens is None:
             raise ValueError("At least one of the arguments: expected_types and expected_tokens should not be None")
         if expected_types is not None and expected_tokens is not None and (len(expected_types) != len(expected_tokens)):
@@ -82,7 +82,7 @@ class CompilationEngine:
             self.tokenizer.curr_index = initial_token_index
         return res
 
-    def _or_compiling(self, compile_methods) -> List[Element]:
+    def _or_compiling(self, compile_methods) -> Union[List[Element], None]:
         for compile_method in compile_methods:
             curr_elements = self._compile_safely(compile_method)
             if curr_elements:
@@ -105,14 +105,14 @@ class CompilationEngine:
         else:
             return []
 
-    def _sequence_compiling(self, compile_methods):
+    def _sequence_compiling(self, compile_methods) -> Union[List[Element], None]:
         elements_lists = [self._compile_safely(compile_method) for compile_method in compile_methods]
         if not None in elements_lists:
             return [elem for elements in elements_lists for elem in elements]
         else:
             return None
 
-    def _add_elements(self, root: Element, elements: List[Element]) -> List[Element]:
+    def _add_elements(self, root: Element, elements: List[Element]) -> bool:
         if elements is None:
             return False
         for element in elements:
@@ -121,8 +121,8 @@ class CompilationEngine:
 
     # compile methods
 
-    def compile_class(self) -> List[
-        Element]:  # TODO: this class should be normal compile method that returns a list of elements and it should have a wrapper
+    def compile_class(self) -> Union[List[Element], None]: # TODO: this class should be normal compile method that
+        # returns a list of elements and it should have a wrapper
         """Compiles a complete class."""
         class_root = Element("class")
 
@@ -142,7 +142,7 @@ class CompilationEngine:
         else:
             return None
 
-    def compile_class_var_dec(self) -> List[Element]:
+    def compile_class_var_dec(self) -> Union[List[Element], None]:
         """Compiles a static declaration or a field declaration."""
         var_dec_root = Element("classVarDec")
 
@@ -158,7 +158,7 @@ class CompilationEngine:
         else:
             return None
 
-    def compile_subroutine(self) -> List[Element]:
+    def compile_subroutine(self) -> Union[List[Element], None]:
         """Compiles a complete method, function, or constructor."""
         subroutine_root = Element("subroutineDec")
 
@@ -178,7 +178,7 @@ class CompilationEngine:
         else:
             return None
 
-    def _inner_compile_parameter_list(self) -> List[Element]:
+    def _inner_compile_parameter_list(self) -> Union[List[Element], None]:
         """Compiles a (possibly empty) parameter list, not including the
         enclosing "()".
         """
@@ -192,13 +192,13 @@ class CompilationEngine:
         else:
             return None
 
-    def compile_parameter_list(self) -> List[Element]:
+    def compile_parameter_list(self) -> Union[List[Element], None]:
         # ((type varName) (',' type varName)*)?
         parameter_list_root = Element("parameterList")
         self._add_elements(parameter_list_root, self._question_mark_compiling(self._inner_compile_parameter_list))
         return [parameter_list_root]
 
-    def compile_var_dec(self) -> List[Element]:
+    def compile_var_dec(self) -> Union[List[Element], None]:
         """Compiles a var declaration."""
 
         var_dec_root = Element("varDec")
@@ -223,10 +223,10 @@ class CompilationEngine:
         "{}".
         """
         statements_root = Element("statements")
-        self._add_elements(self._asterisk_compiling(self.compile_statement()))
+        self._add_elements(statements_root, self._asterisk_compiling(self.compile_statement()))
         return statements_root
 
-    def compile_do(self) -> List[Element]:
+    def compile_do(self) -> Union[List[Element], None]:
         """Compiles a do statement."""
         do_root = Element("doStatement")
         valid_do_statement = True
@@ -238,7 +238,7 @@ class CompilationEngine:
         else:
             return None
 
-    def _compile_normal_subroutine_call(self) -> List[Element]:
+    def _compile_normal_subroutine_call(self) -> Union[List[Element], None]:
 
         # subroutineName '(' expressionList ')'
 
@@ -252,7 +252,7 @@ class CompilationEngine:
         else:
             return None
 
-    def _compile_class_subroutine_call(self) -> List[Element]:
+    def _compile_class_subroutine_call(self) -> Union[List[Element], None]:
 
         # (className | varName) '.' subroutineName '(' expressionList ')'
 
@@ -273,11 +273,11 @@ class CompilationEngine:
         else:
             return None
 
-    def _compile_subroutine_call(self) -> List[Element]:
+    def _compile_subroutine_call(self) -> Union[List[Element], None]:
         # normalSubroutineCall | classSubroutineCall
         return self._or_compiling([self._compile_normal_subroutine_call, self._compile_class_subroutine_call])
 
-    def _compile_array_accessor(self) -> List[Element]:
+    def _compile_array_accessor(self) -> Union[List[Element], None]:
         left_bracket_element = self._add_token_if(expected_token="[")
         expression_element = self.compile_expression()
         right_bracket_element = self._add_token_if(expected_token="]")
@@ -285,7 +285,7 @@ class CompilationEngine:
             return [left_bracket_element, expression_element, right_bracket_element]
         return None
 
-    def compile_let(self) -> List[Element]:
+    def compile_let(self) -> Union[List[Element], None]:
         """Compiles a let statement."""
         let_root = Element("letStatement")
         valid_let_statement = True
@@ -301,7 +301,7 @@ class CompilationEngine:
         else:
             return None
 
-    def compile_while(self) -> List[Element]:
+    def compile_while(self) -> Union[List[Element], None]:
         """Compiles a while statement."""
         while_root = Element("whileStatement")
         valid_while_statement = True
@@ -318,7 +318,7 @@ class CompilationEngine:
         else:
             return None
 
-    def compile_return(self) -> List[Element]:
+    def compile_return(self) -> Union[List[Element], None]:
         """Compiles a return statement."""
         return_root = Element("whileStatement")
 
@@ -333,7 +333,7 @@ class CompilationEngine:
         else:
             return None
 
-    def _compile_else(self) -> List[Element]:
+    def _compile_else(self) -> Union[List[Element], None]:
 
         # ('else' '{' statements '}')
         else_elements = self._add_token_if(expected_token="else")
@@ -346,7 +346,7 @@ class CompilationEngine:
         else:
             return None
 
-    def compile_if(self) -> List[Element]:
+    def compile_if(self) -> Union[List[Element], None]:
         """Compiles a if statement, possibly with a trailing else clause."""
         if_root = Element("ifStatement")
 
@@ -390,7 +390,7 @@ class CompilationEngine:
         else:
             return None
 
-    def compile_expression(self) -> List[Element]:
+    def compile_expression(self) -> Union[List[Element], None]:
         """Compiles an expression."""
         expression_root = Element("expression")
 
@@ -404,7 +404,7 @@ class CompilationEngine:
         else:
             return None
 
-    def compile_term(self) -> List[Element]:
+    def compile_term(self) -> Union[List[Element], None]:
         """Compiles a term. 
         This routine is faced with a slight difficulty when
         trying to decide between some of the alternative parsing rules.
@@ -416,7 +416,7 @@ class CompilationEngine:
         """
         # integerConstant | stringConstant | keywordConstant | varName | varName '['expression']' | subroutineCall |
         # '(' expression ')' | unaryOp term
-        self._or_compiling([
+        return self._or_compiling([
             self._add_token_if(expected_type=TokenTypes.INT_CONST),
             self._add_token_if(expected_type=TokenTypes.STRING_CONST),
             self._compile_keyword_constant(),
@@ -452,10 +452,10 @@ class CompilationEngine:
         self._add_elements(expression_list_root, self._question_mark_compiling(self._inner_compile_expression_list))
         return [expression_list_root]
 
-    def compile_type(self) -> List[Element]:
+    def compile_type(self) -> Union[List[Element], None]:
         return self._add_token_if_or([None, None, None, TokenTypes.IDENTIFIER], ["int", "char", "boolean", None])
 
-    def compile_subroutine_body(self) -> List[Element]:
+    def compile_subroutine_body(self) -> Union[List[Element], None]:
         subroutine_body_root = Element("subroutineBody")
 
         valid_subroutine_body = True
@@ -471,14 +471,14 @@ class CompilationEngine:
         else:
             return None
 
-    def _compile_comma_and_var_name(self) -> List[Element]:
+    def _compile_comma_and_var_name(self) -> Union[List[Element], None]:
         comma_element = self._add_token_if(expected_token=",")
         var_name_element = self._add_token_if(expected_type=TokenTypes.IDENTIFIER)
         if comma_element and var_name_element:
             return comma_element + var_name_element
         return None
 
-    def _compile_comma_and_type_and_var_name(self) -> List[Element]:
+    def _compile_comma_and_type_and_var_name(self) -> Union[List[Element], None]:
         comma_element = self._add_token_if(expected_token=",")
         type_element = self.compile_type()
         var_name_element = self._add_token_if(expected_type=TokenTypes.IDENTIFIER)
