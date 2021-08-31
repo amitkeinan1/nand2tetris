0809b7b547b883a71c5ae91eea66a782c93b4be6
Galo <gofer1505@gmail.com>
2021-08-31 21:11:49 +0300

    fit in page

--- 10/xml_utils.py
+++ 10/xml_utils.py
@@ -1,7 +1,8 @@
 def xml_write_patch(method):
     """
     Full Disclosure: this patch method was taken from stack overflow:
-    `https://stackoverflow.com/questions/16813938/python-print-pretty-xml-create-opening-and-closing-tags-for-empty-tags-text`
+    `https://stackoverflow.com/questions/16813938/
+     python-print-pretty-xml-create-opening-and-closing-tags-for-empty-tags-text`
     """
     def patch_method(self, *args, **kwargs):
         old = self.childNodes
