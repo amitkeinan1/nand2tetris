635e95e900d554bdcdc6253e905b593611248ad7
Amit Keinan <amitk2612@gmail.com>
2021-09-03 22:23:35 +0300

    more cases to extended compiler

--- 11/ExtendedXmlCompiler.py
+++ 11/ExtendedXmlCompiler.py
@@ -36,34 +36,54 @@ class ExtendedXmlCompiler:
     def _generate_tag(category, index, status):
         return f"identifier-{category}-{index}-{status}"
 
-
     def compile(self, write_to_file: bool = True) -> Optional[ElementTree]:
         xml_tree = self.xml_compiler.compile(write_to_file=False)
 
         for element in xml_tree.iter():
-            element_type = self._get_type(element)
-            if element_type == SUBROUTINE_DEC_TAG:
-                self.symbol_table.start_subroutine()
-            elif element_type == "identifier":
-                element.tag = self._get_extended_type(element)
+            if len(element) != 0:  # if this is not a leaf
+                for index, child_element in enumerate(element):
+                    element_type = self._get_type(child_element)
+                    if element_type == SUBROUTINE_DEC_TAG:
+                        self.symbol_table.start_subroutine()
+                    elif element_type == "identifier":
+                        child_element.tag = self._get_extended_type(child_element, index)
 
         if write_to_file:
             xml_tree.write(self.output_path, pretty_print=True)
         else:
             return xml_tree
 
+    def _handle_var_name_defined(self, element, parent):
+        var_name = self._get_name(element)
+        var_kind = VAR_KINDS[self._get_name(parent[0])]
+        self.symbol_table.define(var_name, self._get_name(parent[1]), var_kind)
+        return self._generate_tag(var_kind, self.symbol_table.index_of(var_name), DEFINITION)
+
     # TODO: how can we distinguish between className and varName in subroutine call
     # TODO: add running indexes
-    def _get_extended_type(self, element):
+    def _get_extended_type(self, element: Element, index: int):
         parent = element.getparent()
         parent_type = self._get_type(parent)
+
         if parent_type == CLASS_TAG:
             return self._generate_tag("class", 0, DEFINITION)
+
         elif parent_type == CLASS_VAR_DEC_TAG:
-            var_name = self._get_name(element)
-            var_kind = VAR_KINDS[self._get_name(parent[0])]
-            self.symbol_table.define(var_name, self._get_name(parent[1]), var_kind)
-            return self._generate_tag(var_kind, self.symbol_table.index_of(var_name), DEFINITION)
+            return self._handle_var_name_defined(element, parent)
+
+        elif parent_type == SUBROUTINE_DEC_TAG:
+            if index == 1:
+                return self._generate_tag("class", 0, DEFINITION)
+            else:
+                self.symbol_table.start_subroutine()
+                return self._generate_tag("subroutine", 0, DEFINITION)
+
+        elif parent_type == VAR_DEC_TAG:
+            if index == 1:
+                return self._generate_tag("class", 0, USAGE)
+            else:
+                return self._handle_var_name_defined(element, parent)
+
         else:
             return "identifier"
 
