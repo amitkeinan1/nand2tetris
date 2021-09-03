11f415a216bbc8b85b8c08237c330e8f96a5af9c
Amit Keinan <amitk2612@gmail.com>
2021-09-03 23:46:40 +0300

    even more

--- 11/ExtendedXmlCompiler.py
+++ 11/ExtendedXmlCompiler.py
@@ -53,7 +53,7 @@ class ExtendedXmlCompiler:
         else:
             return xml_tree
 
-    def _handle_var_name_defined(self, element, parent):
+    def _handle_var_defined(self, element, parent):
         var_name = self._get_name(element)
         var_kind = VAR_KINDS[self._get_name(parent[0])]
         self.symbol_table.define(var_name, self._get_name(parent[1]), var_kind)
@@ -67,9 +67,18 @@ class ExtendedXmlCompiler:
             return self._generate_tag("subroutine", 0, USAGE)
         else:
             if index_in_call == 0:
-                return "identifier"  # TODO
+                if self.symbol_table.kind_of(self._get_name(element)) is None:  # TODO: is it good?
+                    return self._generate_tag("class", 0, USAGE)
+                else:
+                    return self._handle_var_used(element)
             if index_in_call == 2:
                 return self._generate_tag("subroutine", 0, USAGE)
+
+    def _handle_var_used(self, element):
+        var_name = self._get_name(element)
+        var_kind = self.symbol_table.kind_of(var_name)
+        var_index = self.symbol_table.index_of(var_name)
+        return self._generate_tag(var_kind, var_index, USAGE)
 
     # TODO: how can we distinguish between className and varName in subroutine call
     # TODO: add running indexes
@@ -81,7 +90,7 @@ class ExtendedXmlCompiler:
             return self._generate_tag("class", 0, DEFINITION)
 
         elif parent_type == CLASS_VAR_DEC_TAG:
-            return self._handle_var_name_defined(element, parent)
+            return self._handle_var_defined(element, parent)
 
         elif parent_type == SUBROUTINE_DEC_TAG:
             if index == 1:
@@ -94,17 +103,25 @@ class ExtendedXmlCompiler:
             if index == 1:
                 return self._generate_tag("class", 0, USAGE)
             else:
-                return self._handle_var_name_defined(element, parent)
+                return self._handle_var_defined(element, parent)
 
         elif parent_type == LET_TAG:
             assert index == 1
-            var_name = self._get_name(element)
-            var_kind = self.symbol_table.kind_of(var_name)
-            var_index = self.symbol_table.index_of(var_name)
-            return self._generate_tag(var_kind, var_index, USAGE)
+            return self._handle_var_used(element)
 
         elif parent_type == DO_TAG:
             return self._handle_subroutine_call(element, index, parent, 1)
+
+        elif parent_type == TERM_TAG:
+            if len(parent) == 1:
+                return self._handle_var_used(element)
+            elif self._get_name(parent[1]) in ["(", "."]:
+                return self._handle_subroutine_call(element, index, parent, 0)
+            elif self._get_name(parent[1]) == "[":
+                assert index == 0
+                return self._handle_var_used(element)
+            else:
+                raise Exception()
 
 
         else:
