85243fdf4442ba0dd5e893d8e6e1792aa92926b1
Amit <amitk2612@gmail.com>
2021-09-05 10:17:27 +0300

    tests

--- 11/amit_tests/unit_tests/unit_tests.py
+++ 11/amit_tests/unit_tests/unit_tests.py
@@ -1,9 +1,13 @@
-from ExtendedXmlCompiler import ExtendedXmlCompiler
+from lxml.etree import Element, ElementTree
+
 from CodeWriter import CodeWriter
+from ExtendedXmlCompiler import ExtendedXmlCompiler
 
 if __name__ == '__main__':
     file_name = "simple_expression"
     compiler = ExtendedXmlCompiler(file_name + ".jack", file_name + ".xml")
-    compiler.compile_expression()
+    expression = compiler.xml_compiler.compile_expression()[0]
+    tree = ElementTree(expression)
+    tree.write(file_name + ".xml", pretty_print=True)
     code_writer = CodeWriter(file_name + ".xml", file_name + ".vm")
-    code_writer.write_code()
+    code_writer.write_expression_code(expression)
